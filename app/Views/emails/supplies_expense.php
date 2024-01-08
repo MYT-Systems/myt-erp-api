c2890c0be378270280bb465e9ed8537aa74ffd70
HeraldIsEthan <85557960+EatmyTan@users.noreply.github.com>
2024-01-08 11:35:00 +0800

    fix - variable naming for email content

--- app/Views/emails/supplies_expense.php
+++ app/Views/emails/supplies_expense.php
@@ -11,7 +11,7 @@
 <body>
     <div style="width: 100%; height: 100%; background-color: #f5f5f5; padding: 20px; box-sizing: border-box;">
         <div style="text-align: end;">
-            <span>SUPPLIES EXPENSE NO. <?= $purchase['id'] ?></span>
+            <span>SUPPLIES EXPENSE NO. <?= $supplies_expense['id'] ?></span>
         </div>
     
         <div style="margin-top: 20px; width: 100%; margin-bottom: 20px; background-color: #fff; padding: 20px; box-sizing: border-box;">
@@ -28,35 +28,35 @@
                 </div>
                 <div style="display: flex; justify-content: space-between; margin-top: 10px;">
                     <div style="font-weight: bold;">Branch:</div>
-                    <div><?= $branch['name'] ?></div>
+                    <div><?= $supplies_expense['branch_name'] ?></div>
                 </div>
                 <div style="display: flex; justify-content: space-between; margin-top: 10px;">
                     <div style="font-weight: bold;">Forwarder:</div>
-                    <div><?= $purchase['forwarder_name'] ?></div>
+                    <div><?= $supplies_expense['forwarder_name'] ?></div>
                 </div>
                 <div style="display: flex; justify-content: space-between; margin-top: 10px;">
                     <div style="font-weight: bold;">Delivery Address:</div>
-                    <div><?= $purchase['delivery_address'] ?></div>
+                    <div><?= $supplies_expense['delivery_address'] ?></div>
                 </div>
             </div>
 
             <div>
                 <div style="display: flex; justify-content: space-between; margin-top: 10px;">
                     <div style="font-weight: bold;">Purchase Date:</div>
-                    <div><?= DATE('M d, Y', strtotime($purchase['supplies_expense_date'])) ?></div>
+                    <div><?= DATE('M d, Y', strtotime($supplies_expense['supplies_expense_date'])) ?></div>
                 </div>
                 <div style="display: flex; justify-content: space-between; margin-top: 10px;">
                     <div style="font-weight: bold;">Delivery Date:</div>
-                    <div><?= DATE('M d, Y', strtotime($purchase['delivery_date'])) ?></div>
+                    <div><?= DATE('M d, Y', strtotime($supplies_expense['delivery_date'])) ?></div>
                 </div>
                 <div style="display: flex; justify-content: space-between; margin-top: 10px;">
                     <div style="font-weight: bold;">Requisitioner:</div>
-                    <div><?= $purchase['requisitioner_name'] ?></div>
+                    <div><?= $supplies_expense['requisitioner_name'] ?></div>
                 </div>
             </div>
 
             <div style="margin-top: 20px; margin-bottom: 20px; background-color: #fff; padding: 20px; box-sizing: border-box;">
-                <span> <?= $purchase['remarks'] ?> </span>
+                <span> <?= $supplies_expense['remarks'] ?> </span>
             </div>
             <div style="margin-top: 20px; margin-bottom: 20px; background-color: #fff; padding: 20px; box-sizing: border-box;">
                 <table style="width: 100%; border-collapse: collapse; border: 1px solid #000;">
@@ -70,7 +70,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php foreach ($purchase_items as $item) : ?>
+                        <?php foreach ($supplies_expense_items as $item) : ?>
                             <tr style="border-top: 1px solid #000;">
                                 <td style="border: 1px solid black;"><?= $item['name'] ?></td>
                                 <td style="border: 1px solid black;"><?= $item['qty'] ?></td>
@@ -81,19 +81,19 @@
                         <?php endforeach; ?>
                         <tr style="border-top: 1px solid #000;">
                             <td colspan="4" style="border: 1px solid black; text-align: right;">Total Amount</td>
-                            <td style="border: 1px solid black;"><?= $purchase['grand_total'] ?></td>
+                            <td style="border: 1px solid black;"><?= $supplies_expense['grand_total'] ?></td>
                         </tr>
                     </tbody>
                 </table>
             </div>
             <div style="margin-top: 20px; margin-bottom: 20px; background-color: #fff; box-sizing: border-box;">
                 <div style="font-weight: bold;" style='color:"#169422";font-family:"Gotham-Rounded-Medium";'> Additional Note: </div>
-                <p><? $purchase['remarks'] ?></p>
+                <p><? $supplies_expense['remarks'] ?></p>
             </div>
             <div style="margin:auto; width: 100%; display: flex; justify-content: space-between; margin-top: 10px; font-weight: bold;">
                 <div style="text-align: center;">
                     <img src="https://i.ibb.co/nngr1WH/signature.png" style="height: 50%; margin-bottom: -50px;z-index: 100;"/>
-                    <p style="text-align: center; font-weight: bold; text-transform: uppercase"><?=  $purchase['approved_by_name'] ?></p>
+                    <p style="text-align: center; font-weight: bold; text-transform: uppercase"><?=  $supplies_expense['approved_by_name'] ?></p>
                     <p>Approved by</p>
                 </div>
             </div>
