e5c6163881a87f6fe5db7147e09a26283e6b57e0
Kian <kian.catubig@module-zero.com>
2025-04-08 09:27:59 +0800

    feat: add project invoice and bank entry IDs to sales and purchase order queries

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -587,6 +587,7 @@ EOT;
 SELECT * FROM (
     SELECT 
         'Credit' AS type,
+        project_invoice.id AS id,
         CONCAT('SALES INVOICE NO. ', project_invoice.invoice_no) AS reference_no,
         project_invoice_payment.payment_date AS date,
         project_invoice_payment.paid_amount,
@@ -602,6 +603,7 @@ SELECT * FROM (
 
     SELECT 
         'Debit' AS type,
+        se_bank_entry.se_id as id,
         CONCAT('PURCHASE ORDER NO. ', se_bank_entry.se_id) AS reference_no,
         se_bank_slip.payment_date AS date,
         se_bank_slip.amount AS paid_amount,
