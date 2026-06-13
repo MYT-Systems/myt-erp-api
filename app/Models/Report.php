066885ab293e9a58754f08353a46178286f71663
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-06-13 17:19:09 +0800

    fix: bank recon opneing balance sums prior transaction instead of rev from curr bal #88

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -536,6 +536,7 @@ EOT;
     {
         $database = \Config\Database::connect();
 
+        /* old opening balance: reverse from current_bal (replaced by forward-from-prior-transactions below)
         $sql = <<<EOT
 SELECT 
     bank.id,
@@ -603,6 +604,61 @@ AND bank.id = ?;
 EOT;
 
         $query = $database->query($sql, [$date_from, $date_from, $date_from, $date_from, $date_from, $date_from, $date_from, $bank_id]);
+        */
+
+        $sql = <<<EOT
+SELECT
+    bank.id,
+    bank.name,
+    bank.current_bal,
+    bank.beginning_bal
+    + IFNULL((
+        SELECT SUM(project_invoice_payment.paid_amount)
+        FROM project_invoice_payment
+        WHERE project_invoice_payment.to_bank_id = bank.id
+        AND project_invoice_payment.is_deleted = 0
+        AND project_invoice_payment.deposit_date < ?
+    ), 0)
+    + IFNULL((
+        SELECT SUM(bank_transfer.amount)
+        FROM bank_transfer
+        WHERE bank_transfer.bank_to_id = bank.id
+        AND bank_transfer.is_deleted = 0
+        AND bank_transfer.transaction_date < ?
+    ), 0)
+    - IFNULL((
+        SELECT SUM(se_bank_slip.amount)
+        FROM se_bank_slip
+        LEFT JOIN se_bank_entry ON se_bank_entry.se_bank_slip_id = se_bank_slip.id
+        WHERE se_bank_slip.bank_from = bank.id
+        AND se_bank_slip.is_deleted = 0
+        AND se_bank_entry.is_deleted = 0
+        AND se_bank_slip.payment_date < ?
+    ), 0)
+    - IFNULL((
+        SELECT SUM(petty_cash_detail.amount)
+        FROM petty_cash_detail
+        LEFT JOIN petty_cash ON petty_cash.id = petty_cash_detail.petty_cash_id
+        WHERE petty_cash_detail.`from` = bank.id
+        AND petty_cash_detail.is_deleted = 0
+        AND petty_cash.is_deleted = 0
+        AND petty_cash_detail.type = 'in'
+        AND petty_cash_detail.date < ?
+    ), 0)
+    - IFNULL((
+        SELECT SUM(bank_transfer.amount)
+        FROM bank_transfer
+        WHERE bank_transfer.bank_from_id = bank.id
+        AND bank_transfer.is_deleted = 0
+        AND bank_transfer.transaction_date < ?
+    ), 0)
+    AS previous_balance
+FROM bank
+WHERE bank.is_deleted = 0
+AND bank.id = ?;
+EOT;
+
+        $query = $database->query($sql, [$date_from, $date_from, $date_from, $date_from, $date_from, $bank_id]);
         return $query ? $query->getRowArray() : false;
     }
 
