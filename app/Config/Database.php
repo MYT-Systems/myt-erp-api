bbdb5ab1e20de39acb173b604732b458316144c7
desirrebarbosa-myt <desirre.barbosa@module-zero.com>
2026-06-23 10:49:40 +0800

    fix: undo db name change #90

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'myt_erp_update',
+        'database' => 'myt_erp',
          'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
